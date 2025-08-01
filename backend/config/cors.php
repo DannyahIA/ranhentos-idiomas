7b155ec41eb90240a8c0baf2c3a9977332bc137b
DannyahIA <danieltavares.mata@gmail.com>
2025-08-01 15:43:57 -0300

    update server

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,24 +2,13 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
+    'allowed_origins' => [
+        'https://ranhentos-idiomas-nazwabgzi-dannyahias-projects.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
